6e698babe213a15f8d1d8d9c031f6fe11a1ca835
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-01-14 12:13:38 +0600

    Coding style fixed

--- src/emessage.c
+++ src/emessage.c
@@ -25,49 +25,51 @@
 #include <Ecore_X.h>
 #include <Ecore_Evas.h>
 
-void exit_all(void* param)
+static void
+exit_all(void *param)
 {
     ecore_main_loop_quit();
 }
 
-static void main_win_resize_handler(Ecore_Evas* main_win)
+static void
+main_win_resize_handler(Ecore_Evas *main_win)
 {
     int w, h;
-    Evas* canvas = ecore_evas_get(main_win);
+    Evas *canvas = ecore_evas_get(main_win);
     evas_output_size_get(canvas, &w, &h);
 
-    Evas_Object* edje = evas_object_name_find(canvas, "edje");
+    Evas_Object *edje = evas_object_name_find(canvas, "edje");
     evas_object_resize(edje, w, h);
 }
 
-static void main_win_key_handler(void* data, Evas* evas,
-                                 Evas_Object* obj, void* event_info)
+static void
+main_win_key_handler(void *data, Evas *evas, Evas_Object *obj, void *event_info)
 {
-    Evas_Event_Key_Up* ev = event_info;
-    if(!strcmp(ev->keyname, "Return"))
+    Evas_Event_Key_Up *ev = event_info;
+    if (!strcmp(ev->keyname, "Return"))
         ecore_main_loop_quit();
 }
 
-int main(int argc, char** argv)
+int
+main(int argc, char **argv)
 {
-    if(argc < 2)
-    {
+    if (argc < 2) {
         fprintf(stderr, "Usage: emessage <message>\n");
         return 1;
     }
 
-    if(!evas_init())
+    if (!evas_init())
         errx(1, "Unable to initialize Evas\n");
-    if(!ecore_init())
+    if (!ecore_init())
         errx(1, "Unable to initialize Ecore\n");
-    if(!ecore_evas_init())
+    if (!ecore_evas_init())
         errx(1, "Unable to initialize Ecore_Evas\n");
-    if(!edje_init())
+    if (!edje_init())
         errx(1, "Unable to initialize Edje\n");
 
     ecore_x_io_error_handler_set(exit_all, NULL);
 
-    Ecore_Evas* main_win = ecore_evas_software_x11_new(0, 0, 0, 0, 600, 800);
+    Ecore_Evas *main_win = ecore_evas_software_x11_new(0, 0, 0, 0, 600, 800);
     ecore_evas_borderless_set(main_win, 0);
     ecore_evas_shaped_set(main_win, 0);
     ecore_evas_title_set(main_win, "emessage");
@@ -75,9 +77,9 @@ int main(int argc, char** argv)
 
     ecore_evas_callback_resize_set(main_win, main_win_resize_handler);
 
-    Evas* main_canvas = ecore_evas_get(main_win);
+    Evas *main_canvas = ecore_evas_get(main_win);
 
-    Evas_Object* edje = edje_object_add(main_canvas);
+    Evas_Object *edje = edje_object_add(main_canvas);
     evas_object_name_set(edje, "edje");
     edje_object_file_set(edje, DATADIR "/emessage/themes/emessage.edj", "emessage");
     evas_object_move(edje, 0, 0);
@@ -101,4 +103,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
